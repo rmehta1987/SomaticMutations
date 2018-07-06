1163bf1a60cfb4ad045176e8a89e91ea2e583a41
Rahul Mehta <rmehta1987@users.noreply.github.com>
2018-07-05 20:19:46 -0500

    ShowIBP
    Simple Binary Matrix generation using Indian Buffet Process, in this case with power law behavior

---
+++ showIBP.m
@@ -0,0 +1,42 @@
+clear themat tempmat 
+cust = 30;
+
+n = 10;
+d = [1,10];
+o=[.2,.5,.9];
+
+for i = 1:cust
+    if i == 1
+        themat(i,1:poissrnd(n)) = 1;
+    else
+        dish = size(themat,2);
+        for k = 1:dish
+            mk = sum(themat(:,k));
+            tryit = (mk - o(1))/(i - 1 + d(1));
+            if tryit > rand
+                themat(i,k)=1;
+            else
+                themat(i,k)=0;
+            end
+        end
+          newd = (n*gamma(1+d(1))*gamma(i-1+d(1)+o(1)))/(gamma(i+d(1))*gamma(d(1)+o(1)));
+            newd = poissrnd(newd);
+            tempmat = zeros(i,newd);
+            tempmat(i,:)=1;
+            themat = horzcat(themat,tempmat);
+    end
+end
+
+
+[r, c] = size(themat);                          % Get the matrix size
+imagesc((1:c)+0.5, (1:r)+0.5, themat);          % Plot the image
+colormap(gray);                              % Use a gray colormap
+axis equal                                   % Make axes grid sizes equal
+
+title('Binary Mutation Representation');
+xlabel('Mutation');
+ylabel('Samples');
+set(gca,  ... % Change some axes properties
+         'XLim', [1 c+1], 'YLim', [1 r+1], ...
+         'GridLineStyle', '-', 'XGrid', 'on', 'YGrid', 'on');
+        
